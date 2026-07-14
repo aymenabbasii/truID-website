167f3fc882e25c61624868b5b1622bb7ddda06aa
hosttakhleeq <host.takhleeq@gmail.com>
2026-07-14 15:25:43 +0500

    Implement Homepage Redesign

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,31 @@ export const metadata = {
 	description:
 		"Transform your customer onboarding with truID's cutting-edge identity verification platform. Fast, secure, and compliant KYC solutions trusted by banks and fintechs to prevent fraud and verify users instantly.",
 }
-
+const DemoVideo = () => {
+	return (
+		<div
+			className="w-full px-10 md:px-16 lg:px-24 py-16 md:py-24 relative"
+			id="demo-video"
+		>
+			<h1 className="text-4xl md:text-6xl font-semibold max-w-3xl text-gray-800 leading-tight mb-6 ml-4 relative z-10">
+				Watch truID in action
+			</h1>
+			<div
+				className="relative max-w-7xl mx-auto rounded-3xl h-[500px] overflow-hidden"
+				id="demo-video"
+			>
+				<video
+					src={"/videos/TruID.mp4"}
+					controls
+					autoPlay
+					className="w-full h-full object-cover"
+				>
+					Your browser does not support the video tag.
+				</video>
+			</div>
+		</div>
+	)
+}
 export default function Home() {
 	return (
 		<div>
@@ -25,6 +49,7 @@ export default function Home() {
 			<TrustedBy />
 			<ImpactSection />
 			<TestimonialCards />
+			<DemoVideo />
 			<CTA />
 			<Footer />
 		</div>
